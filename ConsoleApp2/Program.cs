d06c65b44f9edc24a5b47e1216175bbc4a7efe60
athul <athul.6560@gmail.com>
2022-05-19 17:56:47 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            int r = 5;
+            while(true)
+            Console.WriteLine("Athul");
+        }
+        static void Hello()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
